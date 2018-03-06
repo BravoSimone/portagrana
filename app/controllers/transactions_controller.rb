90905957df079fce118b62bb8cd4e3863cdb2f8d
Simone Bravo <simonebravo96@gmail.com>
2018-03-06 23:56:36 +0100

    Update transaction views and controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -69,6 +69,6 @@ class TransactionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def transaction_params
-      params.fetch(:transaction, {})
+      params.require(:transaction).permit(:reason, :description, :price)
     end
 end
