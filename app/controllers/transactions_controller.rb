d88d7d3239d719de33f560159acc7154530209bd
Simone Bravo <simonebravo96@gmail.com>
2018-03-07 21:49:51 +0100

    Order transactions by creation date

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,7 @@ class TransactionsController < ApplicationController
   # GET /transactions
   # GET /transactions.json
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.order(created_at: :desc)
   end
 
   # GET /transactions/1
